7654a0ab2c113ddc3a102d519ef787cbc5ad38a4
milanm03 <148801002+milanm03@users.noreply.github.com>
2026-02-11 13:24:39 +0100

    Dodat dto

---
+++ server/BusinessLayer/Services/AuthorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLayer.DTOs;
+using CommonLayer.Interfaces;
+
+namespace BusinessLayer.Services
+{
+    public class AuthorService
+    {
+        private IAuthorRepository _authorRepository;
+
+        public AuthorService(IAuthorRepository authorRepository)
+        {
+            this._authorRepository = authorRepository; 
+        }
+
+        public Task<bool>Register(DTORegisterAuthor author)
+        {
+            
+        }
+    }
+}
